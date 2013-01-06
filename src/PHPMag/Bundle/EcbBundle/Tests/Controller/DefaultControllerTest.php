120a89677853080fac4769f5fb8d0c7d85314ca5
Andreas Hucks <andreas.hucks@duochrome.net>
2013-01-06 16:37:11 +0100

    initial commit

--- src/PHPMag/Bundle/EcbBundle/Tests/Controller/DefaultControllerTest.php
+++ src/PHPMag/Bundle/EcbBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testList()
     {
         $client = static::createClient();
+        $crawler = $client->request('GET', '/rates/');
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('tr:contains("USD")')->count());
+        $this->assertEquals(33, $crawler->filter('tbody tr')->count());
+    }
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    /**
+     * @dataProvider provideRates
+     */
+    public function testRate($currency, $expectedRate)
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', sprintf('/rates/%s', $currency));
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter(sprintf('html:contains("%s")', $expectedRate))->count());
+    }
+
+    public static function provideRates()
+    {
+        return array(
+            array('USD', 1.2809),
+            array('RUB', 40.1774),
+            array('ZAR', 11.3448)
+        );
+    }
+
+    public function testInvalidCurrency()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/rates/NONE');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 }
